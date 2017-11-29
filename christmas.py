d7db047a31898e869944469f4e97c731dd476e3b
David Bick <david@drybones.co.uk>
2017-11-29 18:56:30 +0000

    Add env overrides and restrict to December

--- christmas.py
+++ christmas.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from signal import pause
 from random import random
 from gpiozero import LEDBoard
@@ -9,27 +10,31 @@ def twinkle(led_day):
     while True:
 
         now = datetime.datetime.now()
-        day = now.day
-        hour = now.hour
-        minute = now.minute
+        month = int(os.getenv('ADVENT_MONTH') or now.month)
+        day = int(os.getenv('ADVENT_DAY') or now.day)
+        hour = int(os.getenv('ADVENT_HOUR') or now.hour)
+        minute = int(os.getenv('ADVENT_MINUTE') or now.minute)
 
         value = random() if led_day <= day else 0
 
-        if day >= 25:
-            if led_day == 25:
-                value = 1.0 # Light the star constantly
+        if month == 12:
+            if day >= 25:
+                if led_day == 25:
+                    value = 1.0 # Light the star constantly
+                else:
+                    value *= 0.3 # Dim the brighter red LEDs to allow the star to shine
             else:
-                value *= 0.1 # Dim the brighter red LEDs to allow the star to shine
-        else:
-            value *= 0.6 # Dim the LEDs somewhat even when the start isn't lit
+                value *= 0.6 # Dim the LEDs somewhat even when the start isn't lit
 
-        # Turn the lights off overnight, fading in and out
-        if hour <= 5 or hour >= 22:
-            value = 0.0
-        elif hour <= 6:
-            value *= 0.3 ** ((60-minute)/10.0)
-        elif hour >= 21:
-            value *= 0.3 ** (minute/10.0)
+            # Turn the lights off overnight, fading in and out
+            if hour <= 5 or hour >= 22:
+                value = 0
+            elif hour <= 6:
+                value *= 0.3 ** ((60-minute)/10.0)
+            elif hour >= 21:
+                value *= 0.3 ** (minute/10.0)
+        else:
+            value = 0
 
         yield value
 
